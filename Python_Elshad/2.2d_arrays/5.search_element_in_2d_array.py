fb982e4e83bea50e95ba1dabf413cfeb1aa4794b
toaha-unc <toahasiddique.ts@gmail.com>
2025-06-23 11:58:30 -0500

    search element in 2d array

--- Python_Elshad/2.2d_arrays/5.search_element_in_2d_array.py
+++ Python_Elshad/2.2d_arrays/5.search_element_in_2d_array.py
@@ -2,12 +2,16 @@ import numpy as np
 
 my_array = np.array([ [1,2,3],[4,5,6] ])
 
-def traverse(array):
+def search(array, target):
     for i in range(len(array)):
         for j in range(len(array[0])):
-            print(array[i][j])
+            if array[i][j] == target:
+                return [i,j]
+    return -1
+    
+            
 
-traverse(my_array)
+print(search(my_array, 6))
 
 #Time Comp: O(m * n)
 #Space Comp: O(1) 
