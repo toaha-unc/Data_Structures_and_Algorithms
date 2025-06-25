7a03035e107b53ee4a2871069ebddd05c563e157
toaha-unc <toahasiddique.ts@gmail.com>
2025-06-25 11:32:04 -0500

    access and traverse element in list

---
+++ Python_Elshad/3.lists/2.access_element_in_a_list.py
@@ -0,0 +1,24 @@
+
+
+#        0    1    2
+list = ['a', 'b', 'c']
+#       -3    -2   -1
+
+
+print(list[-1])
+
+
+#Time Comp: O(1)
+#Space Comp: O(1)
+
+#Traversing elements of a List 
+for element in list:
+    print(element)
+
+#Traversing elements of a List using index
+for index in range(len(list)):
+    print(list[index])
+
+
+#Time Comp: O(N)
+#Space Comp: O(1)
