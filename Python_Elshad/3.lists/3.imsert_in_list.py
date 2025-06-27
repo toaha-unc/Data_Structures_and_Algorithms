e7b99280ea6cf3acc06b1cc0b1d991336214a447
toaha-unc <toahasiddique.ts@gmail.com>
2025-06-26 19:27:00 -0500

    insert in list

---
+++ Python_Elshad/3.lists/3.imsert_in_list.py
@@ -0,0 +1,17 @@
+
+
+#insert element in a list 
+list = [1,2,3]
+list.insert(0, 0)
+print(list)
+
+#Time Comp: O(1)
+#Space Comp: O(1)
+
+
+#extend a list 
+list.extend([4,5,6])
+print(list)
+
+#Time Comp: O(N)
+#Space Comp: O(N)
